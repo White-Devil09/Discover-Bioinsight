5a73488c6e66fadfa5b06e7d68507ea5111b621a
shashi <es20btech11036>
2023-10-18 20:04:10 +0530

    page push

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,7 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "e3741824f313dc9b23da24ebd4cfe5af31c578a6"
 app.config["MONGO_URI"] = "mongodb+srv://bhanu:bhanu123@cluster1.gpwosqi.mongodb.net/Patient_database?retryWrites=true&w=majority"
+app.config['STATIC_FOLDER'] = 'static'
 
 mongo = PyMongo(app)
 db = mongo.db 
